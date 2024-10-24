3dba280186a929c66c137b84d637b359c4cb7389
Andrew Hollas <andyhollas67@gmail.com>
2024-10-24 12:31:14 -0500

    updates

--- my-blog/src/Blog.js
+++ my-blog/src/Blog.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BlogContext } from './BlogProvider';
+import './App.css';
+
 
 const BlogPage = () => {
     const { blogs, isLoading, error } = useContext(BlogContext);
